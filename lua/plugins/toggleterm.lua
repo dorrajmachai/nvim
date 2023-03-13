64d85261e6722bba9fcf4d28e93a5b08b9b12b3f
dorrajmachai <dwimble22@protonmail.com>
2023-03-13 04:52:11 -0600

    having powershell issues

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,18 +11,6 @@ return {
 	'akinsho/toggleterm.nvim',
 	lazy = false,
 	config = function()
-		local powershell_options = {
-			shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-			shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-			shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-			shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-			shellquote = "",
-			shellxquote = "",
-		}
-		for option, value in pairs(powershell_options) do
-			vim.opt[option] = value
-		end
-
 		require('toggleterm').setup {
 			size = 20,
 			open_mapping = [[<c-\>]],
@@ -33,7 +21,7 @@ return {
 			autochdir = true,
 			persist_size = true,
 			close_on_exit = true,
-		--	shell = vim.o.shell,
+			shell = vim.o.shell,
 			direction = 'horizontal',
 		}
 
