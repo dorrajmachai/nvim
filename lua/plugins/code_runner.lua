a9fd5e42f7fa1f9bd9391d9f1b4df87ed6fe42e4
dorrajmachai <dwimble22@protonmail.com>
2023-03-13 04:29:24 -0600

    adding code-runner

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -12,6 +12,8 @@ return {
 				python = "python $fileName",
 				typescript = "tsc $fileName",
 				lua = "lua $fileName",
+				c = "cd $dir && gcc $fileName -o $fileNameWithoutExt && cd $dir$fileName",
+				cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && cd $dir$fileName",
 			},
 		term = {
 				position = "bot",
